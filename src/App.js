886a4258399c7c64f4e6eccef3cc220af06621e9
Jawel <jawel.mersali@gmail.com>
2021-08-04 10:09:50 +0200

    Commit with subl

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import Decimal from "break_infinity.js";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 
+let score = new Decimal(0);
 function App() {
-  const [score, setScore] = useState(new Decimal(0));
+  // const [score, setScore] = useState(new Decimal(0));
   const [buy, setBuy] = useState(1);
 
   const getScore = () => {
     return score;
   };
 
-  const incrementScore = (score2, gain) => {
-    setScore(score2.plus(gain).floor());
+  const incrementScore = (gain) => {
+    score.plus(gain).floor();
   };
+  // const incrementScore = (score2, gain) => {
+  //   setScore(score2.plus(gain).floor());
+  // };
   const decrementScore = (price) => {
-    setScore(score.minus(price).floor());
+    score.minus(price).floor();
   };
+  // const decrementScore = (price) => {
+  //   setScore(score.minus(price).floor());
+  // };
   return (
     <div className="App">
       <Header score={score.toString()} buy={buy} setBuy={setBuy} />
